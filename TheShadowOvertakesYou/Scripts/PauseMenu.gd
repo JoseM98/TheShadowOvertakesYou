5d11d7d36bce182ff25706de6abd24c6e821c39b
Ruben Rubio Martinez <rubensipala@gmail.com>
2021-01-31 12:37:28 +0100

    v0.19 - merge

--- TheShadowOvertakesYou/Scripts/PauseMenu.gd
+++ TheShadowOvertakesYou/Scripts/PauseMenu.gd
@@ -1,31 +1,18 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Continue_button_up():
-	get_parent().get_parent().visible = false
-	get_parent().get_parent().get_parent().paused = false
+	self.visible = false
+	get_parent().paused = false
 
 
 func _on_Retry_button_up():
+	get_parent().paused = false
 	get_tree().reload_current_scene()
 
 
 func _on_Main_screen_button_up():
+	get_parent().paused = false
 	get_tree().change_scene("res://Scenes/Menus/MainMenu.tscn")
 
 
