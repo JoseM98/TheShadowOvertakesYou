8fa5f2d47c9c760620cdd0d5de55b7e5e00b37c6
Ruben Rubio Martinez <rubensipala@gmail.com>
2021-01-31 11:06:30 +0100

    v0.17 - Torch animation

--- TheShadowOvertakesYou/Scripts/Torch.gd
+++ TheShadowOvertakesYou/Scripts/Torch.gd
@@ -2,6 +2,12 @@ extends Area2D
 
 signal torch_picked
 
+func _process(delta):
+	if $AnimatedSprite.frame == 0:
+		$Light2D.visible = true
+	else:
+		$Light2D.visible = false
+
 func _on_Torch_body_entered(_body: PhysicsBody2D)->void:
 	emit_signal("torch_picked")
 	queue_free()
